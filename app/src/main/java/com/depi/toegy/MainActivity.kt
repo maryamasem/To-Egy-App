135df316ad96893bc6d61980ef92e3e665bd9a30
Maryam Asem <maryamasem05@gmail.com>
2025-10-24 17:27:47 +0300

    initial commit

--- app/src/main/java/com/depi/toegy/MainActivity.kt
+++ app/src/main/java/com/depi/toegy/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ToEgyTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    
+
                 }
             }
         }
