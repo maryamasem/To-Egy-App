b54a34aa1a95054a538e9d6ea34bc2e7ef871c8d
Maryam Asem <maryamasem05@gmail.com>
2025-10-24 21:04:45 +0300

    adding colors

--- app/src/main/java/com/depi/toegy/ui/theme/Color.kt
+++ app/src/main/java/com/depi/toegy/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val NavyBlue = Color(0xFF002B5B)
+
+val Yellow = Color(0xFFFFD200)
+
+val BackgroundWhite = Color(0xFFFFFFFF)
+
+val TextBlack = Color(0xFF333333)
