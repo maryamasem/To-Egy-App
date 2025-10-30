1af6fac94a6d8a1f4bdea9a007c3f500a7a2eaee
mohamedkhafagy <mohamedkhafagy923@gmail.com>
2025-10-30 20:37:01 +0300

    add favorites screen

---
+++ app/src/main/java/com/depi/toegy/Favorites.kt
@@ -0,0 +1,123 @@
+package com.depi.toegy
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+data class FavoritePlace(
+    val name: String,
+    val location: String,
+    val imageRes: Int
+)
+
+@Composable
+fun FavoritesScreen() {
+    val favorites = listOf(
+        FavoritePlace("Pyramids", "Giza", R.drawable.pyramids),
+        FavoritePlace("Khufu", "Giza", R.drawable.kufu)
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFF5F5F5))
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Favorites",
+            fontSize =50.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF002B5B),
+            modifier = Modifier.padding(start = 75.dp, top = 60.dp, bottom = 16.dp)
+        )
+        LazyColumn(verticalArrangement = Arrangement.spacedBy(12.dp)) {
+            items(favorites) { place ->
+                FavoriteCard(place)
+            }
+        }
+    }
+}
+
+@Composable
+fun FavoriteCard(place: FavoritePlace) {
+    var isFavorite by remember { mutableStateOf(true) }
+
+    Card(
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(6.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White),
+        modifier = Modifier.fillMaxWidth()
+            .padding(top = 30.dp, bottom =16.dp)
+    ) {
+        Column {
+            Box {
+                Image(
+                    painter = painterResource(id = place.imageRes),
+                    contentDescription = place.name,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(150.dp)
+                        .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
+                )
+
+                IconButton(
+                    onClick = { isFavorite = !isFavorite },
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(8.dp)
+                        .background(color =Color.White.copy(alpha = 0.5f),CircleShape)
+
+                ) {
+                    Icon(
+                        imageVector = if (isFavorite)
+                            Icons.Filled.Favorite
+                        else
+                            Icons.Outlined.FavoriteBorder,
+                        contentDescription = "Favorite Icon",
+                        tint = if (isFavorite) Color.Red else Color.Gray
+                    )
+                }
+            }
+
+            Column(modifier = Modifier.padding(12.dp)) {
+                Text(
+                    text = place.name,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 18.sp,
+                    color = Color(0xFF1A1A1A)
+                )
+                Text(
+                    modifier = Modifier.padding(top = 6.dp),
+                    text = place.location,
+                    color = Color.Gray,
+                    fontSize = 14.sp
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun FavoritesScreenPreview() {
+    FavoritesScreen()
+}
